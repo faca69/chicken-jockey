54ea8fbb3bbf66dc12ba8a3c4482f051879af604
faca69 <fnikolov123456789@gmail.com>
2025-05-27 18:50:51 +0200

    rough demo for single job

---
+++ src/app/jobs/[id]/page.tsx
@@ -0,0 +1,34 @@
+import { prisma } from "@/lib/prisma";
+
+type JobPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+const JobPage = async ({ params }: JobPageProps) => {
+  const { id } = await params;
+
+  const singleJob = await prisma.job.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!singleJob) {
+    return <div>Job not found</div>;
+  }
+
+  return (
+    <div>
+      <h1> JOB ID: {singleJob.id}</h1>
+      <h1> JOB TITLE: {singleJob.title}</h1>
+      <h1> JOB DESCRIPTION: {singleJob.description}</h1>
+      <h1> CREATED AT: {singleJob.createdAt.toLocaleDateString()}</h1>
+      <h1> UPDATED AT: {singleJob.updatedAt.toLocaleDateString()}</h1>
+      <h1> COMPANY ID: {singleJob.companyId}</h1>
+    </div>
+  );
+};
+
+export default JobPage;
