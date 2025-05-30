bc6a89409f0ff3cdcbb752016cbf9a02b3e8740f
faca69 <fnikolov123456789@gmail.com>
2025-05-31 00:21:20 +0200

    env try 4

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,8 +3,6 @@ import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-
   plugins: [inferAdditionalFields<typeof auth>()],
 });
 
