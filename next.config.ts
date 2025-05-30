c679a8c4170f30b875c0517ef6d33222c4b91969
faca69 <fnikolov123456789@gmail.com>
2025-05-30 23:56:05 +0200

    env try 1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 
   images: {
     remotePatterns: [new URL("https://k7e5kx5bey.ufs.sh/f/**")],
